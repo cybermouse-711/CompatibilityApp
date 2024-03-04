ab79d3786e1aa62ee72e022cbf80208bb7b50099
Liza <desseobscurite@gmail.com>
2024-03-05 00:28:26 +0400

    Adde logic

--- CompatibilityApp/ResultViewController.swift
+++ CompatibilityApp/ResultViewController.swift
@@ -26,18 +26,67 @@ class ResultViewController: UIViewController {
     }
     
     //MARK: - Private Metods
+    private func findValue(for name: String) -> Int {
+        var count = 0
+        
+        for character in name.lowercased() {
+            switch character {
+            case "a", "i", "j", "q", "y":
+                count += 1
+            case "b", "k", "r":
+                count += 2
+            case "c", "g", "l", "s":
+                count += 3
+            case "d", "m", "t":
+                count += 4
+            case "e", "h", "n", "x":
+                count += 5
+            case "u", "v", "w":
+                count += 6
+            case "o", "z":
+                count += 7
+            case "f", "p":
+                count += 8
+            default :
+                count += 0
+            }
+        }
+        return count
+    }
     
-
+    private func findResult() -> Int {
+        var result = 0
+        
+        let yourName = findValue(for: yourName)
+        let parentsName = findValue(for: parentsName)
+        let absDifference = abs(yourName - parentsName)
+        
+        switch absDifference {
+        case 0, 1, 2:
+            result = 100
+        case 3, 4:
+            result = 65
+        case 5:
+            result = 50
+        case 6:
+            result = 40
+        default:
+            result = 30
+        }
+        return result
+    }
 }
 
 //MARK: - Extensions
 private extension ResultViewController {
     func setupUI() {
+        let result = findResult()
+        
         resultNamesLabel.text = "\(yourName ?? "") and \(parentsName ?? "")"
         
         resultProgressView.transform = resultProgressView.transform.scaledBy(x: 1, y: 2)
-        resultProgressView.progress = 2 //text
+        resultProgressView.progress = Float(result) / 100
         
-        procentLabel.text = "" //text
+        procentLabel.text = result.formatted()
     }
 }
