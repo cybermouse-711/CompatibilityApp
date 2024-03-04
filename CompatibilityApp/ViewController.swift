75ede602a4fbb4ab0482a8220ac0a08b80a832ee
Liza <desseobscurite@gmail.com>
2024-03-04 20:51:55 +0400

    Initial Commit

---
+++ CompatibilityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompatibilityApp
+//
+//  Created by Elizaveta Medvedeva on 04.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
