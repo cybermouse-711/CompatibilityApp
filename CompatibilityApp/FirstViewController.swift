4675328cf9c86fdcbfe26a503a6eef7695727048
Liza <desseobscurite@gmail.com>
2024-03-04 23:06:52 +0400

    Outlets initialized

--- CompatibilityApp/FirstViewController.swift
+++ CompatibilityApp/FirstViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+//MARK: - FirstViewController
 class FirstViewController: UIViewController {
 
+    //MARK: - @IBOutlet
+    @IBOutlet var yourNameTF: UITextField!
+    @IBOutlet var parentsNameTF: UITextField!
+    
+    //MARK: - Override Metods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultVC = UIStoryboard as? ResultViewController else { return }
     }
 
-
+    //MARK: - @IBAction
+    @IBAction func resultButton() {
+    }
+    
 }
 
